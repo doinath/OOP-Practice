37674524175e421fd790bd5b04144c2baa86f9d2
nathanael <nathanaeljeddn.delcastillo@gmail.com>
2024-10-15 00:06:37 +0800

    added import java util * and a variable character choice and declared an object for Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
         System.out.print("Hello @User!");
+        var choice = sc.next().charAt(0);
 
     }
 }
